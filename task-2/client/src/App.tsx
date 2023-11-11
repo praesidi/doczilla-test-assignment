12b1d64aa1f3526bc88595823066185e353ed37e
praesidi <edik.halilov1999@gmail.com>
2023-11-11 14:14:47 +0300

    add main page with option buttons

--- task-2/client/src/App.tsx
+++ task-2/client/src/App.tsx
@@ -1,21 +1,45 @@
-import "./assets/styles/style.css";
+import { useState } from 'react';
+import Modal from './components/Modal';
+import './assets/styles/style.css';
 
 function App() {
-	return (
-		<div className="container">
-			<header>
-				<h1>CRUD приложение</h1>
-			</header>
-			<main>
-				<h2>Выберите действие</h2>
-				<div className="buttons-container">
-					<button>Добавить студента</button>
-					<button>Удалить студента</button>
-					<button>Вывести список всех студентов</button>
-				</div>
-			</main>
-		</div>
-	);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [operation, setOperation] = useState<'add' | 'delete' | undefined>();
+
+  function handleAddStudent() {
+    setOperation('add');
+    setIsModalOpen(true);
+  }
+
+  function handleDeleteStudent() {
+    setOperation('delete');
+    setIsModalOpen(true);
+  }
+
+  function handleShowAllStudents() {}
+
+  return (
+    <div className='container'>
+      <header>
+        <h1>CRUD приложение</h1>
+      </header>
+      <main>
+        <h2>Выберите действие</h2>
+        <div className='buttons-container'>
+          <button onClick={handleAddStudent}>Добавить студента</button>
+          <button onClick={handleDeleteStudent}>Удалить студента</button>
+          <button onClick={handleShowAllStudents}>
+            Вывести список всех студентов
+          </button>
+        </div>
+      </main>
+      {isModalOpen ? (
+        <Modal operation={operation} onClose={() => setIsModalOpen(false)} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default App;
