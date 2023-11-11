449bb6e9724da505b610207cb87b6c624a066044
praesidi <edik.halilov1999@gmail.com>
2023-11-11 19:51:58 +0300

    add student card with information about student

--- task-2/client/src/components/StudentList.tsx
+++ task-2/client/src/components/StudentList.tsx
@@ -12,8 +12,17 @@ function StudentList({ students }: { students: IStudent[] }) {
   );
 }
 
-function StudentCard() {
-  return <div></div>;
+function StudentCard({ student, index }: { student: IStudent; index: number }) {
+  return (
+    <div className='student-card'>
+      <span>{index + 1}</span>
+      <span className='name'>{student.firstName}</span>
+      <span className='name'>{student.lastName}</span>
+      <span className='name'>{student.fatherName}</span>
+      <span>{student.group}</span>
+      <span>{student.birthday}</span>
+    </div>
+  );
 }
 
 export default StudentList;
