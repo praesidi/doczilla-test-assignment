dd47963767613330d352ba0f7a52cb535301d221
praesidi <edik.halilov1999@gmail.com>
2023-11-13 16:06:47 +0300

    fix crucial bugs and add sorting by date range

--- task-3/src/App.tsx
+++ task-3/src/App.tsx
@@ -1,77 +1,78 @@
 import { useEffect, useState } from 'react';
-import { ITask } from './types';
-import dayjs from 'dayjs';
+import { IDateRange, ITask } from './types';
 import {
   Box,
   CircularProgress,
   Container,
   Pagination,
-  Typography,
+  // Typography,
 } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import getUnfinishedTasks from './utils/getUnfinishedTasks';
 import useFetch from './hooks/useFetch';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import TaskList from './components/TaskList';
 import Popup from './components/Popup';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import getTasksSortedByDate from './utils/getTasksSortedByDate';
 import getIndexesByPage from './utils/getIndexesByPage';
-
-// TODO: Поиск по дате с помощью календаря (API). ~
-
-// TODO: * Вывод задач по диапазону дат (API). ~
-// TODO: * Поиск с выпадающим списком. ~~~
 
 function App() {
   const [APIParameters, setAPIParameters] = useState<string>('');
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [isShowUnfinished, setIsShowUnfinished] = useState<boolean>(false); //TODO: rename
+  const [areUnfinishedShown, setAreUnfinishedShown] = useState<boolean>(false);
   const [currentSortOption, setCurrentSortOption] = useState<string>('none');
-  const [popupTask, setPopupTask] = useState<ITask | undefined>(); //FIXME: get rid of the state
+  const [popupTask, setPopupTask] = useState<ITask | undefined>();
+  const [dateRange, setDateRange] = useState<IDateRange>({
+    from: undefined,
+    to: undefined,
+  });
+  const { data, isLoading } = useFetch(APIParameters); // data from api. don't mutate it
+  const [currentData, setCurrentData] = useState<ITask[] | null>(null); // data to show. aka page
+  const [sortedData, setSortedData] = useState<ITask[] | null>(null); // data sorted on our side
 
-  const { data, isLoading, error } = useFetch(APIParameters); // data from api don't mutate it
-  const [currentData, setCurrentData] = useState<ITask[] | null>(null); //data to show aka page
-  // const [sortedData, setSortedData] = useState<ITask[] | null>(null);
-
-  const [currentPage, setCurrentPage] = useState<number>(1); // current page
+  const [currentPage, setCurrentPage] = useState<number>(1); // current page num
   const [pageQty, setPageQty] = useState<number>(1); // total number of pages
 
   useEffect(() => {
-    if (data) {
-      setCurrentData(data);
-    }
+    if (data) setSortedData(data);
   }, [data]);
 
   useEffect(() => {
-    if (data && !isShowUnfinished) setPageQty(Math.ceil(data?.length / 20));
-  }, [data, isShowUnfinished]);
+    if (sortedData) setPageQty(Math.ceil(sortedData.length / 10));
+  }, [sortedData]);
 
   useEffect(() => {
-    const [firstItemIndex, lastItemIndex] = getIndexesByPage(currentPage, 20);
-    const pageData = data?.slice(firstItemIndex, lastItemIndex);
+    const [firstItemIndex, lastItemIndex] = getIndexesByPage(currentPage, 10);
+    const pageData = sortedData?.slice(firstItemIndex, lastItemIndex);
 
     if (pageData) setCurrentData(pageData);
-  }, [data, currentPage]);
+  }, [currentPage, sortedData]);
 
-  if (error) {
-    return (
-      <Container
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh',
-          width: '100wv',
-        }}
-      >
-        <Typography variant='h4' color={'#B00020'}>
-          Something went wrong ;( <br /> Try again
-        </Typography>
-      </Container>
-    );
-  }
+  useEffect(() => {
+    setCurrentPage(1);
+
+    if (currentSortOption === 'none' && !areUnfinishedShown) {
+      setSortedData(data);
+    }
+
+    if (currentSortOption === 'none' && areUnfinishedShown) {
+      setSortedData(getUnfinishedTasks(data));
+    }
+
+    if (currentSortOption !== 'none' && areUnfinishedShown) {
+      setSortedData(
+        getTasksSortedByDate(getUnfinishedTasks(data), currentSortOption)
+      );
+    }
+
+    if (currentSortOption !== 'none' && !areUnfinishedShown) {
+      setSortedData(getTasksSortedByDate(data, currentSortOption));
+    }
+  }, [currentSortOption, areUnfinishedShown, data]);
 
   function handlePageChange(page: number) {
     setCurrentPage(page);
@@ -83,22 +84,10 @@ function App() {
 
   function handleSortByDate(option: string) {
     setCurrentSortOption(option);
-    setCurrentPage(1); // resets current page
-    if (option === 'none' && !isShowUnfinished) {
-      setCurrentData(data);
-    }
-
-    if (data && option !== 'none' && !isShowUnfinished) {
-      const sortedByDate = getTasksSortedByDate(data, option);
-      setCurrentData(sortedByDate);
-    }
   }
 
   function handleToggle() {
-    setCurrentPage(1);
-    setCurrentData(data); //TODO: fix
-    if (!isShowUnfinished) getUnfinishedTasks(data);
-    setIsShowUnfinished(!isShowUnfinished);
+    setAreUnfinishedShown(!areUnfinishedShown);
   }
 
   function handleShowTodayTasks() {
@@ -108,7 +97,7 @@ function App() {
     todayStart.setHours(0, 0, 0, 1);
     todayEnd.setHours(23, 59, 59, 999);
 
-    const query = `/date?from=${new Date(todayStart).getTime()}to${new Date(
+    const query = `/date?from=${new Date(todayStart).getTime()}&to=${new Date(
       todayEnd
     ).getTime()}`;
 
@@ -123,10 +112,41 @@ function App() {
     dayWeekAgo.setHours(0, 0, 0, 1);
     today.setHours(23, 59, 59, 999);
 
-    const query = `/date?from=${new Date(dayWeekAgo).getTime()}to${new Date(
+    const query = `/date?from=${new Date(dayWeekAgo).getTime()}&to=${new Date(
       today
     ).getTime()}`;
 
+    console.log(query);
+
+    setAPIParameters(query);
+  }
+
+  function handleShowTaskInRange(dateRange: IDateRange) {
+    setDateRange(dateRange);
+
+    let fromDate = new Date(
+      new Date().setFullYear(new Date().getFullYear() - 5)
+    );
+    fromDate.setHours(0, 0, 0, 1); // default value
+
+    let toDate = new Date();
+    toDate.setHours(23, 59, 59, 999); // default value
+
+    if (dateRange.from) {
+      fromDate = new Date(dateRange.from);
+      fromDate.setHours(0, 0, 0, 1);
+    }
+
+    if (dateRange.to) {
+      toDate = new Date(dateRange.to);
+      toDate.setHours(23, 59, 59, 999);
+    }
+
+    const query = `/date?from=${new Date(fromDate).getTime()}&to=${new Date(
+      toDate
+    ).getTime()}`;
+
+    console.log(query);
     setAPIParameters(query);
   }
 
@@ -139,19 +159,18 @@ function App() {
     setPopupTask(task);
   }
 
-  // function handleOnSelectedDay() {}
-
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Container disableGutters={true} sx={{ height: '100vh', width: '100%' }}>
         <Header onSearch={setAPIParameters} />
         <Container disableGutters sx={{ display: 'flex', py: '30px' }}>
           <Sidebar
-            isShowUnfinished={isShowUnfinished}
+            areUnfinishedShown={areUnfinishedShown}
             onToggle={handleToggle}
             onToday={handleShowTodayTasks}
             onThisWeek={handleShowWeekTasks}
-            date={dayjs(new Date())}
+            onDateRange={handleShowTaskInRange}
+            dateRange={dateRange}
           />
           {isLoading ? (
             <Box
@@ -171,13 +190,14 @@ function App() {
                 tasks={currentData}
                 sortOption={currentSortOption}
                 onSortByDate={handleSortByDate}
+                // onSortByDate={handleSortedData}
                 onShowAll={handleShowAll}
                 handleTaskCardClick={handleTaskCardClick}
               />
               <Pagination
                 count={pageQty}
                 page={currentPage}
-                onChange={(e, page) => handlePageChange(page)}
+                onChange={(_, page) => handlePageChange(page)}
                 sx={{
                   mt: '30px',
                   width: '100%',
@@ -193,5 +213,4 @@ function App() {
     </LocalizationProvider>
   );
 }
-
 export default App;
