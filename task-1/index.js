500f5ea48b511cf3cda7b9555deef85cc42e71a9
praesidi <edik.halilov1999@gmail.com>
2023-11-14 13:41:54 +0300

    add new functionality

--- task-1/index.js
+++ task-1/index.js
@@ -1,10 +1,10 @@
-import fs from "fs";
+import fs, { rmSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const fileNamesList = {};
 
-function getAllTextFileNames(directory) {
+function findAllTextFileNames(directory) {
 	if (fs.readdirSync(directory).length < 1) return null;
 
 	fs.readdirSync(directory).forEach((file) => {
@@ -17,20 +17,82 @@ function getAllTextFileNames(directory) {
 		}
 
 		if (isFolder) {
-			getAllTextFileNames(filePath);
+			findAllTextFileNames(filePath);
 		}
 	});
 
 	return fileNamesList;
 }
 
-function concatTextFiles(directoryPath, outputFileName) {
-	getAllTextFileNames(directoryPath);
+const dependencyList = [];
 
+function buildDependencyList() {
+	Object.keys(fileNamesList).forEach((fileName, index) => {
+		const fileContent = fs.readFileSync(fileNamesList[fileName], "utf-8");
+		const matches = fileContent.match(/require `(.*?)`/g);
+		if (matches) {
+			matches.forEach((match) => {
+				const dependency = match.replace(/['"`]+/g, "").match(/[^/]*$/)[0];
+				const isLinked = dependencyList.some(
+					(item) => item[dependency] === fileName
+				);
+				if (!isLinked) {
+					dependencyList.push({ [fileName]: dependency });
+				} else {
+					throw "Linked Dependency";
+				}
+			});
+		}
+	});
+	// console.log("dependencyList: ", dependencyList);
+}
+
+function sortFiles() {
 	const sortedFileNames = Object.keys(fileNamesList).sort();
+	const resultArr = [...sortedFileNames];
+
+	sortedFileNames.forEach((fileName, index) => {
+		const fileDependency = dependencyList.filter((item) => item[fileName]);
+
+		if (fileDependency.length === 1) {
+			const indexOfDependency = sortedFileNames.indexOf(
+				fileDependency[0][fileName]
+			);
+
+			[resultArr[index], resultArr[indexOfDependency]] = [
+				resultArr[indexOfDependency],
+				resultArr[index],
+			];
+		}
+
+		if (fileDependency.length > 1) {
+			let dependencyIndex = 0;
+
+			fileDependency.forEach((item) => {
+				const lowestDependencyIndex = sortedFileNames.indexOf(item[fileName]);
+
+				if (dependencyIndex < lowestDependencyIndex) {
+					dependencyIndex = lowestDependencyIndex;
+				}
+			});
+
+			[resultArr[index], resultArr[dependencyIndex]] = [
+				resultArr[dependencyIndex],
+				resultArr[index],
+			];
+		}
+	});
+	return resultArr;
+}
+
+function concatTextFiles(directoryPath, outputFileName) {
+	findAllTextFileNames(directoryPath);
+	buildDependencyList();
+
+	const sortedFiles = sortFiles();
 	let concatenatedContent = "";
 
-	sortedFileNames.forEach((fileName) => {
+	sortedFiles.forEach((fileName) => {
 		const fileContent = fs.readFileSync(fileNamesList[fileName], "utf8");
 		concatenatedContent += fileContent;
 	});
